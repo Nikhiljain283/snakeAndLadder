60ed6277f888eee10117ff2c8d18ec96f80cc81e
Nikhiljain283 <nikhiljainsgrl283@gmail.com>
2022-08-22 19:41:01 +0530

    Added : start position

---
+++ AtStartPosition.java
@@ -0,0 +1,10 @@
+package com.snakeladder;
+
+public class AtStartPosition {
+	public static void main(String[] args) {
+		
+		int playerPosition = 0;
+		System.out.println("Player position at start = "+playerPosition);
+		
+	}
+}
