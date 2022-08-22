1281ff2ca791c32bd0e71ea754ed57f5954a1e3d
Nikhiljain283 <nikhiljainsgrl283@gmail.com>
2022-08-22 19:44:20 +0530

    Added : number appearing on dice

---
+++ NumberOnDice.java
@@ -0,0 +1,10 @@
+package com.snakeladder;
+
+public class NumberOnDice {
+	public static void main(String[] args) {
+		
+		int diceNum = (int)(Math.random()*6)+1;
+		System.out.println("Number on dice : "+diceNum);
+	}
+
+}
